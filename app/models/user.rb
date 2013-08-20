7d6ea815860458bd73d2a323f57eb35cdbc54c5b
Alison Rowland <alisonrowland@fastmail.fm>
2013-08-19 23:07:18 -0400

    Adding agency role for relevant email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :name
 
+  after_create :add_roles
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
@@ -12,5 +14,11 @@ class User < ActiveRecord::Base
          user.name = auth['info']['name'] || user.email
       end
     end
+  end
+
+  def add_roles
+    self.add_role :admin if User.count == 1 # make the first user an admin
+
+    self.add_role :agency if %w{ .gov .mil }.any? {|d| self.email.end_with?(d)}
   end
 end
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,16 +1,19 @@
+require 'spec_helper'
+
 describe "Home", :type => :feature do
   before do
     @user_attrs = {:name => 'Joe Citizen', :email => 'joe@citizen.org'}
   end
+
   context "when a user is logged in" do
     before do
       user = User.create!(@user_attrs)
       current_user = user
     end
 
-    it 'should show them a link to find personalized opportunities' do
+    it 'should show them a link to find opportunities relevant to them' do
       visit '/'
-      page.should have_content 'Find Opportunities'
+      page.should have_content('Relevant to me')
     end
   end
 
@@ -19,11 +22,6 @@ describe "Home", :type => :feature do
       before do
         user = User.create!(@user_attrs)
         current_user = user
-      end
-
-      it 'should show them a link to find personalized opportunities' do
-
-
       end
     end
   end
