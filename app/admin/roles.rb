3c78a8717896c02984499cff88196c36834dc5f0
Alison Rowland <alisonrowland@fastmail.fm>
2013-08-22 11:08:22 -0400

    Permitting Role and User parameters to be edited

--- app/admin/roles.rb
+++ app/admin/roles.rb
@@ -1,2 +1,7 @@
 ActiveAdmin.register Role do
+  controller do
+    def permitted_params
+      params.permit(:role => [:name, :resource_type, :resource_id])
+    end
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,2 +1,7 @@
 ActiveAdmin.register User do
+  controller do
+    def permitted_params
+      params.permit(:user => [:name, :email])
+    end
+  end
 end
