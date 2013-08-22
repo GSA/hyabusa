edb375fa8f73ef52438c8a706bc9b42112ee2836
Alison Rowland <alisonrowland@fastmail.fm>
2013-08-21 23:19:32 -0400

    AAAAARGH. The problem was in the Heroku config. Moving ROLES back into application.yml.

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,5 @@ module Mybusa
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    ROLES = %w{ admin user VIP }
   end
 end
