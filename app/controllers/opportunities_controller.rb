2d5b39937e0828a9b41dfa451863613f875a9fc0
Amos Stone <amos@amosarts.com>
2013-08-23 10:43:09 -0400

    Partial commit towards new MO xp

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,11 +1,11 @@
 class OpportunitiesController < ApplicationController
 	def index
-		@opportunities = HTTParty.get("http://mo.tynsax.com/api/registrations")
-		
+		@opportunities = HTTParty.get(ENV['MODUS_OPERANDI_HOME']+"/api/operations")
 	end
 
 	def show_mo
-		@opportunity = HTTParty.get("http://mo.tynsax.com/api/registrations/#{params[:mo_id]}/full_map")
+		# @opportunity = HTTParty.get(ENV['MODUS_OPERANDI_HOME']+"/api/operations/#{params[:mo_id]}/map")
 		# render :json => @opportunity
+		
 	end
 end
