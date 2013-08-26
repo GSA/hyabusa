1d81b03dc79ab7baf16e7c4714a6b597e0ca201f
Alison Rowland <alisonrowland@fastmail.fm>
2013-08-26 14:43:46 -0400

    Adding initial Profile class

---
+++ app/models/profile.rb
@@ -0,0 +1,3 @@
+class Profile < ActiveRecord::Base
+  belongs_to :entity_type
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130823152749) do
+ActiveRecord::Schema.define(version: 20130823192350) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 20130823152749) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "profiles", force: true do |t|
+    t.string   "contact_first_name"
+    t.string   "contact_last_name"
+    t.string   "contact_email"
+    t.integer  "entity_type_id"
+    t.integer  "num_employees"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "profiles", ["entity_type_id"], name: "index_profiles_on_entity_type_id"
 
   create_table "roles", force: true do |t|
     t.string   "name"
