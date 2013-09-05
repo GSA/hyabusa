512b2307f9606860bc75c4a46e451a4b99145076
Alison Rowland <alison.rowland@businessusa.gov>
2013-09-05 16:44:17 -0400

    Added missing schema link from profile_person to profile.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130904145730) do
+ActiveRecord::Schema.define(version: 20130904195559) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20130904145730) do
     t.string   "phone_mobile"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "profile_id"
   end
 
   create_table "profiles", force: true do |t|
