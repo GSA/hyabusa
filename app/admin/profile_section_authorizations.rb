7bf5041914b5c824918aa9012d3b2760a8c3732c
Alison Rowland <alison.rowland@businessusa.gov>
2013-09-17 12:18:52 -0400

    Got ProfileSectionAuthorizations and profile sidebar working

--- app/admin/profile_section_authorizations.rb
+++ app/admin/profile_section_authorizations.rb
@@ -1,7 +1,16 @@
 ActiveAdmin.register ProfileSectionAuthorization do
   controller do
     def permitted_params
-      params.permit(:profile_section_authorization => [:id, :profile_section_id, :is_admin_approved, :is_pra_pending, :is_pra_approved, :omb_control_no, :pra_approved_on])
+      params.permit(:profile_section_authorization => [
+        :id,
+        :profile_section_id,
+        :is_admin_approved,
+        :is_pra_pending,
+        :is_pra_approved,
+        :omb_control_no,
+        :pra_approved_on,
+        :pra_applies
+      ])
     end
   end
 end
--- app/models/profile_section.rb
+++ app/models/profile_section.rb
@@ -1,5 +1,17 @@
 class ProfileSection < ActiveRecord::Base
   has_many :fields, class_name: 'ProfileField'
-  has_one :profile_section
+  has_one :authorization, class_name: ProfileSectionAuthorization
   accepts_nested_attributes_for :fields, allow_destroy: true
+
+  scope :admin_approved, -> { joins(:authorization).where(profile_section_authorizations: { is_admin_approved: true }) }
+  scope :pra_approved, -> { joins(:authorization).where(profile_section_authorizations: { is_pra_approved: true }) }
+  scope :user_fillable, -> { joins(:authorization).where(profile_section_authorizations: { pra_applies: true }) }
+
+  def self.visible(sandbox=false)
+    if sandbox
+      self.admin_approved.user_fillable
+    else
+      self.admin_approved.pra_approved.user_fillable
+    end
+  end
 end
