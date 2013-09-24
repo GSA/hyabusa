07caab9131de0107eb3cda87c48f62394fa0a827
Amos Stone <amos@amosarts.com>
2013-09-24 11:46:45 -0400

    [#26] completes call to action for profile in site root

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 describe "Home", :type => :feature do
   before do
     @user_attrs = {:name => 'Joe Citizen', :email => 'joe@citizen.org'}
+    @profile_attrs = { :num_employees => 40, :company_name => 'Go Go Gadgets', :address1 => '4000 Park Ave', :city => 'Washington', :state => 'DC', :postal_code => '20009', :org_type => "Service Provider" }
   end
 
   context "when a user is logged in" do
-    before do
-      user = User.create!(@user_attrs)
-    end
 
-    it 'should show them a link to find opportunities relevant to them' do
+    it "show the user's business profile" do
       login_with_myusa
+      profile = Profile.new(@profile_attrs)
+      profile.user = current_user
+      profile.save
       visit '/'
-      page.should have_content('Relevant to me')
+      page.should have_content('My BusinessUSA Profile')
     end
   end
 end
